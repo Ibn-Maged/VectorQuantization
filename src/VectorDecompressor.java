1bfe5aa0b1f85e806b7afe2a60a08197cca09ea4
hassan1876 <95700641+hassan1876@users.noreply.github.com>
2023-12-04 01:40:21 +0200

    added base methods in decompressor

--- src/VectorDecompressor.java
+++ src/VectorDecompressor.java
@@ -1,2 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class VectorDecompressor {
+    HashMap<Integer,String> codeBook;
+    Integer image[][];
+    public void compress(){
+        read("");
+        for(int i = 0;i<image.length;i++){
+            for(int j=0;j<image[i].length;j++){
+
+            }
+        }
+    }
+    public void read(String fileName){
+
+    }
+    public void write(){
+
+    }
 }
