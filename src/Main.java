c96c05eec33ba2e2225d97f3a1bc25ecdab25025
Ibn-Maged <mohamedmaged1607@gmail.com>
2023-12-03 22:42:48 +0200

    Added image splitting to blocks

--- src/Main.java
+++ src/Main.java
@@ -1,73 +1,64 @@
 import java.util.ArrayList;
 
-//public class Main {
-//
-//    public static void main(String[] args) {
-//        VectorCompressor vc = new VectorCompressor();
-//        ArrayList<ArrayList<Integer>> image = new ArrayList<>();
-//        for(int i = 0; i < 8; i++){
-//            for(int j = 0; i < 8; i++){
-//                image.add(i, new ArrayList<>());
-//                image.get(i).add(j);
-//            }
-//        }
-//
-//        vc.image = image;
-//        vc.compress(4, 2, 2);
-//    }
-//}
-
 public class Main {
 
-    public static int[][][] splitArray(int[][] originalArray, int n) {
-        int rows = originalArray.length;
-        int cols = originalArray[0].length;
-
-        int subarrayRows = rows / n;
-        int subarrayCols = cols / n;
-
-        int[][][] subarrays = new int[subarrayRows][subarrayCols][n * n];
-
-        for (int i = 0; i < subarrayRows; i++) {
-            for (int j = 0; j < subarrayCols; j++) {
-                for (int x = 0; x < n; x++) {
-                    for (int y = 0; y < n; y++) {
-                        subarrays[i][j][x * n + y] = originalArray[i * n + x][j * n + y];
-                    }
-                }
-            }
-        }
-
-        return subarrays;
-    }
-
     public static void main(String[] args) {
-        int[][] originalArray = {
-                {1, 2, 3, 4, 5, 6},
-                {5, 6, 7, 8, 5, 6},
-                {9, 10, 11, 12, 5, 6},
-                {13, 14, 15, 16, 5, 6},
-                {13, 14, 15, 16, 5, 6},
-                {13, 14, 15, 16, 5, 6}
-        };
         VectorCompressor vc = new VectorCompressor();
-//        vc.image = originalArray;
-        vc.compress(2, 0, 0);
-        vc.getAverage(0,0,0,0);
-//        int n = 2;  // Size of the subarrays
-//        int[][][] result = splitArray(originalArray, n);
-//
-//        // Print the result
-//        for (int[][] subarrayRows : result) {
-//            for (int[] subarray : subarrayRows) {
-//                for (int value : subarray) {
-//                    System.out.print(value + " ");
-//                }
-//                System.out.println();
-//            }
-//            System.out.println();
-//        }
-
-
+        vc.compress(0, 3, 2);
     }
 }
+
+//public class Main {
+//
+//    public static int[][][] splitArray(int[][] originalArray, int n) {
+//        int rows = originalArray.length;
+//        int cols = originalArray[0].length;
+//
+//        int subarrayRows = rows / n;
+//        int subarrayCols = cols / n;
+//
+//        int[][][] subarrays = new int[subarrayRows][subarrayCols][n * n];
+//
+//        for (int i = 0; i < subarrayRows; i++) {
+//            for (int j = 0; j < subarrayCols; j++) {
+//                for (int x = 0; x < n; x++) {
+//                    for (int y = 0; y < n; y++) {
+//                        subarrays[i][j][x * n + y] = originalArray[i * n + x][j * n + y];
+//                    }
+//                }
+//            }
+//        }
+//
+//        return subarrays;
+//    }
+//
+//    public static void main(String[] args) {
+//        int[][] originalArray = {
+//                {1, 2, 3, 4, 5, 6},
+//                {5, 6, 7, 8, 5, 6},
+//                {9, 10, 11, 12, 5, 6},
+//                {13, 14, 15, 16, 5, 6},
+//                {13, 14, 15, 16, 5, 6},
+//                {13, 14, 15, 16, 5, 6}
+//        };
+//        VectorCompressor vc = new VectorCompressor();
+////        vc.image = originalArray;
+//        vc.compress(2, 0, 0);
+//        vc.getAverage(0,0,0,0);
+////        int n = 2;  // Size of the subarrays
+////        int[][][] result = splitArray(originalArray, n);
+////
+////        // Print the result
+////        for (int[][] subarrayRows : result) {
+////            for (int[] subarray : subarrayRows) {
+////                for (int value : subarray) {
+////                    System.out.print(value + " ");
+////                }
+////                System.out.println();
+////            }
+////            System.out.println();
+////        }
+//
+//
+//    }
+//}
--- src/VectorCompressor.java
+++ src/VectorCompressor.java
@@ -3,58 +3,40 @@ import java.util.ArrayList;
 public class VectorCompressor {
     int[][] image = {
             {1, 2, 3, 4, 5, 6},
-            {5, 6, 7, 8, 5, 6},
-            {9, 10, 11, 12, 5, 6},
-            {13, 14, 15, 16, 5, 6},
-            {13, 14, 15, 16, 5, 6},
-            {13, 14, 15, 16, 5, 6}
+            {7, 8, 9, 10, 11, 12},
+            {13, 14, 15, 16, 17, 18},
+            {19, 20, 21, 22, 23, 24},
+            {25, 26, 27, 28, 29, 30},
+            {31, 32, 33, 34, 35, 36}
     };
-    int[][][] vectors = new int[3][3][4];
-    int codebookSize = 2;
+    int[][][] vectors;
+    public void compress(int codebookSize, int vectorLength, int vectorWidth){
+        int numOfBlocks = image.length * image[0].length / (vectorLength * vectorWidth);
+        vectors = new int[numOfBlocks][vectorLength][vectorWidth];
 
-    public void compress(int codebookSize, int vectorSizeX, int vectorSizeY){
-        int rows = image.length;
-        int cols = image[0].length;
-        this.codebookSize = codebookSize;
-
-        int subarrayRows = rows / codebookSize;
-        int subarrayCols = cols / codebookSize;
-
-//        int[][][] subarrays = new int[subarrayRows][subarrayCols][codebookSize * codebookSize];
-
-        for (int i = 0; i < subarrayRows; i++) {
-            for (int j = 0; j < subarrayCols; j++) {
-                for (int x = 0; x < codebookSize; x++) {
-                    for (int y = 0; y < codebookSize; y++) {
-                        vectors[i][j][x * codebookSize + y] = image[i * codebookSize + x][j * codebookSize + y];
-                    }
+        for(int k = 0, x = 0, y = 0; k < numOfBlocks; k++){
+            for(int i = 0; i < vectorLength; i++){
+                for(int j = 0; j < vectorWidth; j++){
+                    vectors[k][i][j] = image[x + i][y + j];
                 }
             }
-        }
-
-    }
-
-    void getAverage(int x1, int x2, int y1, int y2){
-        double[][] average = new double[codebookSize][codebookSize];
-        for(int i = 0; i < codebookSize; i++){
-            for(int j = 0; j < codebookSize; j++){
-                average[i][j] = 0;
+            y = (y + vectorWidth) % image[0].length;
+            if(y == 0){
+                x += vectorLength;
             }
         }
-        for(int i = 0; i < vectors.length; i++){
-            for(int j = 0; j < vectors[i].length; j++){
-                for(int k = 0; k < vectors[i][j].length; k++){
-                    average[j][k] += vectors[i][j][k];
+
+        for(int k = 0; k < vectors.length; k++){
+            for(int i = 0; i < vectorLength; i++){
+                for (int j = 0; j < vectorWidth; j++){
+                    System.out.print(vectors[k][i][j] + " ");
                 }
+                System.out.println();
             }
-        }
-        for(int i = 0; i < codebookSize; i++){
-            for(int j = 0; j < codebookSize; j++){
-                average[i][j] /= vectors.length;
-                System.out.println(average[i][j]);
-            }
+            System.out.println();
         }
     }
+
 
     public void read(String filePath){
 
